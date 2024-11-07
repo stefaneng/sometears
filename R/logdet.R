b0e1e4c701b454f03df7f8820220ac9dda32440c
Stefan Eng <engs@umich.edu>
2024-11-07 06:00:48 -0700

    Prefix logdet functions with h

--- R/logdet.R
+++ R/logdet.R
@@ -3,13 +3,13 @@
 # DAGMA: Learning DAGs via M-matrices and a Log-Determinant Acyclicity Characterization (arXiv:2209.08037).
 # arXiv. https://doi.org/10.48550/arXiv.2209.08037
 #' @export
-log_det <- function(X, s = 1) {
+h_logdet <- function(X, s = 1) {
   d <- ncol(X)
   -log(det(s * diag(d) - X * X)) + d * log(s)
 }
 
 #' @export
-log_det_grad <- function(X, s = 1) {
+h_logdet_grad <- function(X, s = 1) {
   d <- ncol(X)
   2 * solve(t(s * diag(d) - X * X)) * X
 }
--- tests/testthat/test-logdet.R
+++ tests/testthat/test-logdet.R
@@ -1,6 +1,6 @@
 test_that("logdet works", {
   X <- matrix(0, nrow = 3, ncol = 3)
 
-  expect_equal(log_det(lower.tri(X)), 0)
-  expect_true(all(log_det_grad(lower.tri(X)) == 0))
+  expect_equal(h_logdet(lower.tri(X)), 0)
+  expect_true(all(h_logdet_grad(lower.tri(X)) == 0))
 })
