796b9d7024fc1f2bd1bdfe56692b9591644b7ead
Stefan Eng <engs@umich.edu>
2024-11-19 17:41:31 -0500

    Refactor set size into different function

---
+++ R/utils_topo.R
@@ -0,0 +1,13 @@
+topo_init_min_size <- function(d, model_type = c('linear', 'nonlinear')) {
+  model_type <- match.arg(model_type)
+  if (model_type == "linear") {
+    round(min(1.5 * d, min(sqrt(d) * (d - 1) * log10(d), d * (d - 1) / 2)))
+  }
+}
+
+topo_init_max_size <- function(d, model_type = c('linear', 'nonlinear')) {
+  model_type <- match.arg(model_type)
+  if (model_type == "linear") {
+    round(max(1.5 * d, min(sqrt(d) * (d - 1) * log10(d), d * (d - 1) / 2)))
+  }
+}
